fc731c8d6e44d69d44e6f045fcd74986b6950691
Nestor <nesti_dj@hotmail.com>
2023-04-26 07:20:53 -0300

    Primer Commit Ej1

---
+++ CarpetaHenry/EjercicioMod3.js
@@ -0,0 +1,18 @@
+//String
+var bootcamp = 'Henry';
+
+//Numeros
+var numeroEntero = 22;
+var numeroNegativo = -14;
+var numeroDecimal= 15.16;
+
+//Booleanos
+var estoyEntendiendo = true;
+var estoyAburrido = false;
+
+//Undefined
+var cajon1;
+console.log(cajon1);
+
+//Null
+var sinNada = null;
