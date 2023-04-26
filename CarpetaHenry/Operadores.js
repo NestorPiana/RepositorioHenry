2fd3c75c14cee3344c83531c9639c8389e89d4a2
Nestor <nesti_dj@hotmail.com>
2023-04-26 09:03:30 -0300

    Operadores

---
+++ CarpetaHenry/Operadores.js
@@ -0,0 +1,18 @@
+//Suma
+console.log(3 + 5);
+
+//Resta
+console.log(7 - 3);
+
+//Multiplicacion
+console.log(5 * 8);
+
+//Division
+console.log(30 / 9);
+
+//Resto
+console.log(95 % 4);
+
+//Prueba de Precedencia
+console.log(3 + 5 * 2 - 8);
+
